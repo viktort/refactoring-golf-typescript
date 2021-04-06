ed644edeec10bb3bd8ef3c0bb8f49efbbe97acd2
viktort <viktor.trako@cazoo.co.uk>
2021-04-06 16:49:03 +0100

    mob next [ci-skip]

---
+++ src/kata/golf/hole4/money.ts
@@ -0,0 +1,19 @@
+import { Incalculable } from "./incalculable";
+
+export class Money {
+    public value: number;
+    public currency: string;
+
+
+    constructor(value: number, currency: string) {
+        this.value = value;
+        this.currency = currency;
+    }
+
+    plus(other: Money): Money {
+        if (other.currency !== this.currency) {
+            throw new Incalculable();
+        }
+        return new Money(this.value + other.value, other.currency);
+    }
+}
